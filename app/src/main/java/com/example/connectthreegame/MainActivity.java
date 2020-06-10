9f3bdf07882a7b166a5f649645d3f54f262c95a6
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2020-06-10 00:23:36 -0700

    added game pieces that fall into place upon clicking the board

--- app/src/main/java/com/example/connectthreegame/MainActivity.java
+++ app/src/main/java/com/example/connectthreegame/MainActivity.java
@@ -3,10 +3,36 @@ package com.example.connectthreegame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private boolean afroTurn = true;
+
+    public void placeGamePiece(View view) {
+        ImageView counter = (ImageView) view;   // just get an image view of the view to access resources
+
+
+        counter.getTag();
+
+        Log.i("tag", counter.getTag().toString());
+
+
+        counter.setTranslationY(-1000);         // place view off of screen
+
+        if(afroTurn){
+            counter.setImageResource(R.drawable.afro_player);  // set game piece
+            afroTurn = false;
+        } else {
+            counter.setImageResource(R.drawable.anime_player);  // set game piece
+            afroTurn = true;
+        }
+
+
+        counter.animate().translationYBy(1000).rotation(3600).setDuration(1000);    // move into place
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
